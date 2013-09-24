7c0e5b745ac4b21979fe4510cb7794bde1b4c74c
ashley williams <heyashleyashley@gmail.com>
2013-09-24 17:14:35 -0400

    add format_number

---
+++ beginner/format_number/format_number.rb
@@ -0,0 +1,10 @@
+# Write a method `format` for the class integer that converts the number to a
+# string and adds commas at the appropriate places
+
+class Integer
+
+  def format
+    #code goes here...
+  end
+
+end
