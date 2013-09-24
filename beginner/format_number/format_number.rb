8b26f815a5f6f1f0ddc8ada81d1b87de3cbd8b23
ashley williams <heyashleyashley@gmail.com>
2013-09-24 17:16:22 -0400

    add testing instructions for format_number todo

--- beginner/format_number/format_number.rb
+++ beginner/format_number/format_number.rb
@@ -1,6 +1,8 @@
 # Write a method `format` for the class integer that converts the number to a
 # string and adds commas at the appropriate places
 
+#To test your solution run `rspec format_number_spec.rb` in your terminal
+
 class Integer
 
   def format
