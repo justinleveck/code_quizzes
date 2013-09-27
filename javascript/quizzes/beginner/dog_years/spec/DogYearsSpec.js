af852a2483175c49af87ebce29662366a6836a68
ashley williams <heyashleyashley@gmail.com>
2013-09-27 15:37:45 -0400

    add javascript dogyears todo

--- javascript/quizzes/beginner/dog_years/spec/DogYearsSpec.js
+++ javascript/quizzes/beginner/dog_years/spec/DogYearsSpec.js
@@ -1,13 +1,28 @@
-describe( "Dice#roll", function() {
-  it("should return an number", function() {
-    expect(typeof Dice.roll()).toBe("number")
-  });      
+describe("Dog", function () {
+  dog = new Dog();
 
-  it("should return a number greater than 0", function() {
-    expect(Dice.roll()).toBeGreaterThan(0)
-  });  
+  describe("#age", function() {
+    it("should return an Integer", function(){
+      expect(typeof dog.age).toEqual('number');
+    });
+  });
 
-  it("should return a number less than 7", function() {
-    expect(Dice.roll()).toBeLessThan(7)
-  });  
+  describe("#age_in_years", function() {
+    it("should return an Integer", function(){
+      expect(typeof dog.age_in_years()).toEqual('number');
+    });
+  });
+
+  describe("#age_in_days", function() {
+    it("should return the dog's age in years times 365", function(){
+      expect(dog.age_in_days()).toEqual(dog.age_in_years() * 365);
+    });
+  });
+
+  describe("#age_in_dog_years", function() {
+    it("should return the dog's age in years times 7", function(){
+      expect(dog.age_in_dog_years()).toEqual(dog.age_in_years() * 7);
+    });
+  });
+
 });
